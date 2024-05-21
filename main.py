5f2a1171a9d64a21cf0c7d7ef6722061e6a997b7
Ivan Danilau <ivdanilov96@gmail.com>
2024-05-22 00:45:31 +0200

    add sample

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import hashlib
 import hmac
 import json
 
-# обычно для подписи jwt используются более надежные секреты, например RSA ключи.
+# обычно для подписи JWT используются более надежные секреты, например RSA ключи.
 # Но для объяснения принципов работы JWT это не имеет значения
 SECRET = "mysecret"
 
 
+# функция кодирования строки (в нашем случае это части JWT) в base64 кодировку
 def base64_encoded(source: bytes) -> str:
     # '=' в конце, или padding связан с особенностями  работы base64,
     # и служит для обеспечения кратности данных, подлежащих кодировке.
-    # Для jwt принято использовать кодировку base64 без padding
+    # Для JWT принято использовать кодировку base64 без padding
     return base64.urlsafe_b64encode(source).rstrip(b'=').decode()
 
 
@@ -24,51 +25,49 @@ def base64_decoded(encoded_str: str) -> str:
 
 
 # Заголовок в JSON формате, закодированный в base64.
-def get_header() -> str:
-    header = json.dumps({"alg": "HS256",
-                         "typ": "JWT"})
+def get_base64_encoded_header() -> str:
+    header = json.dumps({"alg": "HS256", "typ": "JWT"})  # пример заголовка
     print(f"Header info: {header}")
-    encoded_header = base64_encoded(header.encode())
+    encoded_header = base64_encoded(header.encode())  # Кодируем в base64
     print(f"Encoded header: {encoded_header}")
     return encoded_header
 
 
 # Полезная нагрузка в JSON формате, закодированная в base64.
-def get_payload() -> str:
-    payload = json.dumps({
-        # данные могут быть любыми
+def get_base64_encoded_payload() -> str:
+    payload = json.dumps({  # пример полезной нагрузки, данные могут быть любыми
         "sub": "user1234",
         "exp": int(datetime.datetime(2100, 1, 1).timestamp())
     })
     print(f"Payload info: {payload}")
-    encoded_header = base64_encoded(payload.encode())
-    print(f"Encoded payload: {encoded_header}")
-    return encoded_header
+    encoded_payload = base64_encoded(payload.encode())  # Кодируем в base64
+    print(f"Encoded payload: {encoded_payload}")
+    return encoded_payload
 
 
 # Получаем подпись с помощью HMAC функции, которая использует секрет и данные, содержащиеся в header и payload
-def get_signature(unsigned_jwt: str, secret: str) -> str:
+def get_signature(base64_encoded_header: str, base64_encoded_payload: str, secret: str) -> str:
+    # Получили JWT, он еще не подписан, но это уже JWT токен.
+    unsigned_jwt = base64_encoded_header + "." + base64_encoded_payload
+    # применение hmc для вычисления подписи
     signature = hmac.new(secret.encode(), unsigned_jwt.encode(), hashlib.sha256).digest()
-    encoded_signature = base64_encoded(signature)
+    encoded_signature = base64_encoded(signature)  # Кодируем в base64
     print(f"Signature: {encoded_signature}")
     return encoded_signature
 
 
 if __name__ == '__main__':
-    header = get_header()
-    payload = get_payload()
-    unsigned_jwt = header + "." + payload
-    # Получили JWT, он еще не подписан, но это уже jwt токен.
-    print("Unsigned jwt: " + unsigned_jwt)
-    signature = get_signature(unsigned_jwt, SECRET)
-    signed_jwt = unsigned_jwt + "." + signature
-    # Можно скопировать значение токена и проверить его на сайте https://jwt.io/, указав правильный секрет
-    print("Signed jwt: " + signed_jwt)
+    base64_encoded_header = get_base64_encoded_header()
+    base64_encoded_payload = get_base64_encoded_payload()
+    base64_encoded_signature = get_signature(base64_encoded_header, base64_encoded_payload, SECRET)
+    # Подписываем токен, путем присоединения закодированных частей через точку
+    signed_jwt = base64_encoded_header + "." + base64_encoded_payload + "." + base64_encoded_signature
+    # Можно скопировать значение токена и проверить его подпись на сайте https://jwt.io/, указав правильный секрет
+    print("Signed JWT: " + signed_jwt)
     # Демонстрация того, что любой может прочитать данные из токена, без необходимости знать секрет
-    without_signature = signed_jwt[:signed_jwt.rfind(".")]
+    without_signature = signed_jwt[:signed_jwt.rfind(".")]  # обрезаем подпись
     print(f"I still can see the data: {base64_decoded(without_signature)}")
-    # Демострация того, что для верификации подписи необходимо знание секрета
-    new_signature = get_signature(unsigned_jwt, "invalid secret")
-    print(f"Try to get signature: {signature}")
-    print(f"Signature are equal: {signature == new_signature}")
-    print(f"Final jwt: {signed_jwt}")
+    # Демонстрация того, что для верификации подписи необходимо знание секрета
+    new_signature = get_signature(base64_encoded_header, base64_encoded_payload, "invalid secret")
+    print(f"Signatures are equal: {base64_encoded_signature == new_signature}")
+    print(f"Final JWT: {signed_jwt}")
